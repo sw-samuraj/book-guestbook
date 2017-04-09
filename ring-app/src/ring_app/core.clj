5f967938df817385f680408109b516dc4653a896
Vít Kotačka <vit.kotacka@gmail.com>
2017-04-09 21:11:28 +0200

    Reload and no-cache middlewares have been added.

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,6 +1,7 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request-map]
   (response/response
@@ -8,7 +9,14 @@
          (:remote-addr request-map)
          "</body></html>")))
 
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragme"] "no-cache"))))
+
 (defn -main []
-  (jetty/run-jetty handler
-                   {:port 3000
-                    :join? false}))
+  (jetty/run-jetty
+    (-> #'handler wrap-nocache wrap-reload)
+    {:port 3000
+     :join? false}))
